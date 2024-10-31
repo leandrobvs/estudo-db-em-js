10b905ec5ccd5b528282830cf7b7497e605652e6
leandrobvs <112587531+leandrobvs@users.noreply.github.com>
2024-10-31 14:38:56 -0300

    v002

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const statement =
   'create table author (id number, name string, age number, city string, state string, country string)';
 
-// console.log(statement);
+let regexp = /\w+ \w+ (\w+) \((.+)\)/;
 
-let regexp = /\w+ (\w+) (\w+) \((.+)\)/;
+let parsedStatement = regexp.exec(statement);
 
-let result = regexp.exec(statement);
+let tableName = parsedStatement[1];
+let columns = parsedStatement[2].split(', ');
 
-let tableName = result[2];
-let columns = result[3].split(', ');
+const database = {
+  tables: {
+    [tableName]: {
+      columns: {},
+      data: []
+    }
+  }
+};
 
-console.log(tableName);
-console.log(columns);
+for (let i = 0; i < columns.length; i++) {
+  let values = columns[i].split(' ');
+  database.tables[tableName].columns[values[0]] = values[1];
+}
+
+console.log(JSON.stringify(database, null, ' '));
