a88d0057e08d51a4d9b701fbb5e86d0549309448
leandrobvs <112587531+leandrobvs@users.noreply.github.com>
2024-11-02 01:33:47 -0300

    v005

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const DatabaseError = function (statement, message) {
+  this.command = statement.split(' ')[0];
+  this.error = message;
+};
+
 const database = {
   tables: {},
 
@@ -27,19 +32,45 @@ const database = {
       this.tables[tableName].columns[key] = value;
     }
   },
+  insert(statement) {
+    let regexp = /\w+ \w+ (\w+) \((.+)\) \w+ \((.+)\)/;
+    let parsedStatement = regexp.exec(statement);
+
+    let tableName = parsedStatement[1];
+    let columns = parsedStatement[2].split(', ');
+    let values = parsedStatement[3].split(', ');
+
+    let row = {};
+
+    for (let i = 0; i < columns.length; i++) {
+      row[columns[i]] = values[i];
+    }
+
+    this.tables[tableName].data.push(row);
+  },
 
   execute(statement) {
     if (statement.startsWith('create')) {
       return this.createTable(statement);
     }
-    if (statement.startsWith('delete')) {
-      console.log('command not implemented yet');
+    if (statement.startsWith('insert')) {
+      return this.insert(statement);
+    } else {
+      throw new DatabaseError(statement, 'Unknown Command');
     }
   }
 };
 
-database.execute(
-  'create table author (id number, name string, age number, city string, state string, country string)'
-);
-database.execute('delete table author');
-console.log(JSON.stringify(database, null, ' '));
+try {
+  database.execute(
+    'create table author (id number, name string, age number, city string, state string, country string)'
+  );
+  database.execute('insert into author (id, name, age) values (1, Douglas Crockford, 62)');
+  database.execute('insert into author (id, name, age) values (2, Linus Torvalds, 47)');
+  database.execute('insert into author (id, name, age) values (3, Martin Fowler, 54)');
+  console.log(JSON.stringify(database, null, ' '));
+} catch (error) {
+  console.log(error);
+}
+
+// 6. Insira o objeto em "data".
